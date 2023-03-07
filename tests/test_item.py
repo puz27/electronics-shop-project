093341761a2daae9b78be18f143f783b0ed37a11
anv <anv@woori.ru>
2023-03-07 17:25:32 +0300

    add metods and tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+@pytest.fixture
+def test_object():
+    return Item("Potato", 100.5, 50)
+
+def test_item_calculate_total_price(test_object):
+    assert test_object.calculate_total_price() == 5025.0
+
+
+def test_item_apply_discount(test_object):
+    test_object.apply_discount()
+    assert test_object.price == 100.5
