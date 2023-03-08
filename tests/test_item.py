67401880c475b51d8aa6ff000327c68ea0d88228
Nick_Linux <n.avramenko87@gmail.com>
2023-03-08 21:04:55 +0300

    add test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,9 +1,11 @@
 from src.item import Item
 import pytest
 
+
 @pytest.fixture
 def test_object():
     return Item("Potato", 100.5, 50)
+
 
 def test_item_calculate_total_price(test_object):
     assert test_object.calculate_total_price() == 5025.0
